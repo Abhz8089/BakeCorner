98eaddecb66b6ffc22f176712bd614173b9c76ef
Abhijith <abhijithabhiz8089@gmail.com>
2023-06-17 10:11:38 +0530

    second

--- routes/admin.js
+++ routes/admin.js
@@ -20,8 +20,8 @@ const verifyLogin=(req,res,next)=>{
   if(req.session.admin && req.session.admin.loggedIn){
     next()
   }else{
-    // res.redirect('/admin/adminLogin')
-    next()
+    res.redirect('/admin/adminLogin')
+    
   }
 }
 
